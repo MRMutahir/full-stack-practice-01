4449d99d0eff4c2021bc17e9db048e6e3430d551
Muhammad Mutahir <mutahirkareem820@gmail.com>
2024-11-04 20:02:20 +0500

    add nodemailer  config

---
+++ server/src/config/mail.ts
@@ -0,0 +1,20 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_SERVER,
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: process.env.SMTP_LOGIN,
+    pass: process.env.SMTP_PASSWORD
+  }
+});
+
+export const sendEmail = async (to: string, subject: string, body: string) => {
+  await transporter.sendMail({
+    from: process.env.FROM_EMAIL,
+    to: to,
+    subject: subject,
+    html: body
+  });
+};
