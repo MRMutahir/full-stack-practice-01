1db294ad2818a3f92395bd92de986cc9e2c2605d
Muhammad Mutahir <mutahirkareem820@gmail.com>
2024-11-14 18:45:55 +0500

    create RegisterForm components and add SubmitBtn

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,12 @@
-import HeroSection from "@/components/base/HeroSection"
-import { Button } from "@/components/ui/button"
+import HeroSection from "@/components/base/HeroSection";
+import SubmitBtn from "@/components/common/SubmitBtn";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       {/* <Button variant="outline">Click me</Button> */}
-      <HeroSection/>
+      <HeroSection />
     </div>
-  )
+  );
 }
